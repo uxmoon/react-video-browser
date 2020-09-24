86570f7f63f7bdc46fc935043b3296161a036f46
Martin Luna <martin.luna@possible.com>
2020-09-24 10:41:21 -0300

    added event handlers on search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,28 @@
 import React from "react";
 
 class SearcBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // call cb from parent component
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Video search</label>
-            <input type="text" />
+            <input
+              value={this.state.term}
+              onChange={this.onInputChange}
+              type="text" />
           </div>
         </form>
       </div>
